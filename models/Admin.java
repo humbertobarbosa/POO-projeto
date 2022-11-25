11f95d1dcd4fc912a1b5dc9742ff23e80752f7b1
arthwrvl <joaobob63@gmail.com>
2022-11-25 20:10:47 -0300

    feat: add user-classes

---
+++ models/Admin.java
@@ -0,0 +1,7 @@
+package models;
+
+public class Admin extends User{
+    private String AccessKey;
+
+
+}
---
+++ models/Client.java
@@ -0,0 +1,16 @@
+package models;
+
+import java.util.Date;
+
+public class Client extends User{
+    private Cart cart;
+
+
+    public Client(String name, String email, String password, Date birthDate){
+        super(name, email, password, birthDate);
+        cart = new Cart();
+    }
+    public void addToCart(Product item){
+        cart.addItem(item);
+    }
+}
